2bf1cdaaae493f9f22c0cb053cc382447cc6222c
-thugnasty- <eterictodd@gmail.com>
2018-08-05 11:33:49 -0600

    Updated basic file structure for react app.

--- super-smashed-app/src/App.js
+++ super-smashed-app/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const My404 = () => {
+  return(
+    <div>
+      <h1>404'd!</h1>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <main>
+      <Switch>
+      <Route component={My404} /> 
+      </Switch>
+    </main>
+  )
 }
 
 export default App;
