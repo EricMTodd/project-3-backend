07fe7be0594e60eb7bbc20fdd7322b64123a676a
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-08-08 10:54:54 -0600

    Code maintenance

--- super-smashed-app/src/BattleContainer/BattleContainer.js
+++ super-smashed-app/src/BattleContainer/BattleContainer.js
@@ -11,43 +11,6 @@ class BattleContainer extends Component {
         }
     }
 
-    // componentDidMount() {
-    //     this.getBattles().then((battles) => {
-    //         this.setState({
-    //             battles: battles.data
-    //         })
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     });
-    // }
-
-    // getBattles = async () => {
-    //     const battles = await fetch("http://localhost:9000/api/v1/battles", {
-    //         credentials: "include",
-    //         mtehod: "GET"
-    //     }) ;
-    //     const parsedBattles = battles.json();
-    //     return parsedBattles
-    // }
-
-    // addBattle = async (battle, e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const createBattle = await fetch("http://localhost:9000/api/v1/battles", {
-    //             method: "post",
-    //             credentials: "include",
-    //             body: JSON.stringify(battle),
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             }
-    //         });
-    //         const parsedResponse = await createBattle.json();
-    //         this.setState({battles: [...this.props.battles, parsedResponse.data]})
-    //     } catch (err) {
-    //         console.log(err)
-    //     }
-    // }
-
     render() {
         return (
             <div>
--- super-smashed-app/src/UserContainer/UserContainer.js
+++ super-smashed-app/src/UserContainer/UserContainer.js
@@ -12,43 +12,6 @@ class UserContainer extends Component {
         console.log(this.props)
     }
 
-    // componentDidMount() {
-    //     this.getFighters().then((fighters) => {
-    //         this.setState({
-    //             fighters: fighters.data
-    //         })
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     });
-    // }
-
-    // getFighters = async () => {
-    //     const fighters = await fetch("http://localhost:9000/api/v1/fighters", {
-    //         credentials: "include",
-    //         mtehod: "GET"
-    //     }) ;
-    //     const parsedFighters = fighters.json();
-    //     return parsedFighters
-    // }
-
-    // addFighter = async (fighter, e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const createFighter = await fetch("http://localhost:9000/api/v1/fighters", {
-    //             method: "post",
-    //             credentials: "include",
-    //             body: JSON.stringify(fighter),
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             }
-    //         });
-    //         const parsedResponse = await createFighter.json();
-    //         this.setState({fighters: [...this.props.fighters, parsedResponse.data]})
-    //     } catch (err) {
-    //         console.log(err)
-    //     }
-    // }
-
     render() {
         return (
             <div>
